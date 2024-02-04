9d5d463bbb0a180e5869f09e02d427656339fa63
eguzmanfgsdfg <153762731+eguzmanfgsdfg@users.noreply.github.com>
2024-02-04 02:33:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def handle_jlogin___():
    return  ({'token':'encoded_jwt'})
 
+@app.route('/v1/token', methods=['GET'])
+def handle_jlogin():
+   content_type = request.headers.get('Content-Type')
+   print (request.headers.get('username'))  
+   print (request.headers.get('password'))  
+   #resp = sp(request.headers.get('username'),request.headers.get('password'))
+   resp =''
+   print ('+++++++++++++++++el sp devolvio  [',resp,']')
+   if resp=='' :
+      return  ({'error':'Datos Invalidos','name':request.headers.get('username')})
+   encoded_jwt =getToken(request.headers.get('username'),resp)
+   return  ({'token':encoded_jwt})
+
 if __name__ == '__main__':
    print("+++++++++++++++++++++++++++++++++++ main")
    app.run(debug=False, port=4000)
